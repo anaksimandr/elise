1db331c73a73eed453f85e66807292405bcdd5ac
s.andreenko@gmail.com <s.andreenko@gmail.com@ee834c96-7cc9-813e-71c8-7042ef674424>
2012-03-02 16:04:39 +0000

    working on plugins support

--- core/src/modules.cpp
+++ core/src/modules.cpp
@@ -40,6 +40,35 @@ PluginLoader::~PluginLoader()
 
 int PluginLoader::getPluginsList()
 {
+	//-- Static plugins? not intresting
+	//foreach (QObject *plugin, QPluginLoader::staticInstances())
+	//	populateMenus(plugin);
+
+	QDir pluginsDir = QDir(qApp->applicationDirPath());
+
+	//-- Really not shure that this is needed
+#if defined(Q_OS_WIN)
+	if (pluginsDir.dirName().toLower() == "debug" || pluginsDir.dirName().toLower() == "release")
+		pluginsDir.cdUp();
+#elif defined(Q_OS_MAC)
+	if (pluginsDir.dirName() == "MacOS") {
+		pluginsDir.cdUp();
+		pluginsDir.cdUp();
+		pluginsDir.cdUp();
+	}
+#endif
+
+	pluginsDir.cd("Plugins");
+
+	//-- Trying loading each file in dir as plugin
+	foreach (QString fileName, pluginsDir.entryList(QDir::Files)) {
+		QPluginLoader loader(pluginsDir.absoluteFilePath(fileName));
+		QObject* plugin = loader.instance();
+		if (plugin) {
+			populateMenus(plugin);
+			pluginFileNames += fileName;
+		}
+	}
 
 	return 0;
 }
