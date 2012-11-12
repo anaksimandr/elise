95635078c0b15eec8c5b4b5babfaa0365c04f055
s.andreenko@gmail.com <s.andreenko@gmail.com@ee834c96-7cc9-813e-71c8-7042ef674424>
2012-11-12 18:28:46 +0000

    refactoring

---
+++ trunk/core/src/elise.h
@@ -0,0 +1,11 @@
+#ifndef ELISE_CORE_ELISE_H_
+#define ELISE_CORE_ELISE_H_
+
+#include <stdint.h>
+
+namespace core
+{
+int shutDown(intptr_t result, intptr_t);
+} //namespace core
+
+#endif // ELISE_CORE_ELISE_H_
