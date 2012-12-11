8643f904b1c24609a6fefc082c80f032c3204d34
s.andreenko@gmail.com <s.andreenko@gmail.com@ee834c96-7cc9-813e-71c8-7042ef674424>
2012-12-11 20:52:09 +0000

    Added license terms

--- core/src/modules/pluginloader/pluginloaderoptions.cpp
+++ core/src/modules/pluginloader/pluginloaderoptions.cpp
@@ -1,3 +1,20 @@
+/*  Elise IM, free crossplatform IM client
+	Copyright (C) 2012  Elise project
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <QtWidgets/QtWidgets>
 #include "pluginloaderoptions.h"
 #include "../../core.h"
@@ -42,7 +59,7 @@ int PluginLoaderOptions::createLoaderOptionsPage(intptr_t pfnPageAdder, intptr_t
 	treeView_ = new QTreeView(widget);
 	treeView_->resize(472, 250);
 	treeView_->move(3, 3);
-	PluginsTreeModel* model = new PluginsTreeModel(treeView_);
+	PluginsTreeModel* model = new PluginsTreeModel(treeView_, widget);
 	treeView_->setModel(model);
 	treeView_->setColumnWidth(0, 18);
 	treeView_->setColumnWidth(1, 168);
